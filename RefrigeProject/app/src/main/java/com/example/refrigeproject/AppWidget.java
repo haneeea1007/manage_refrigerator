68ab69af35ed3568b45fd40728fed593a005a192
haneeea <haneeea@gmail.com>
2019-12-14 20:07:32 +0900

    add widget

--- RefrigeProject/app/src/main/java/com/example/refrigeproject/AppWidget.java
+++ RefrigeProject/app/src/main/java/com/example/refrigeproject/AppWidget.java
@@ -1,9 +1,13 @@
 package com.example.refrigeproject;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.example.refrigeproject.show_foods.AddFoodActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,10 +17,16 @@ public class AppWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setImageViewResource(R.id.imgWidget, R.drawable.frigewidgetico);
+
+        // 위젯 레이아웃을 클릭하면 인텐트를 통해 이동함
+        Intent intent = new Intent(context, AddFoodActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.imgWidget, pendingIntent);
+
+        // 들어갔다가 완료하면 나오도록 하기
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
