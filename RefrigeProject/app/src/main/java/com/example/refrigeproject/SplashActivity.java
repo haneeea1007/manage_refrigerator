e773c54a72dc76d536e881c94528272c4342ed64
haneeea <haneeea@gmail.com>
2019-12-18 11:12:12 +0900

    splash activity add

--- RefrigeProject/app/src/main/java/com/example/refrigeproject/SplashActivity.java
+++ RefrigeProject/app/src/main/java/com/example/refrigeproject/SplashActivity.java
@@ -1,19 +1,31 @@
 package com.example.refrigeproject;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, KakaoLoginActivity.class);
-        startActivity(intent);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), KakaoLoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
 
-        finish();
+//        Intent intent = new Intent(this, KakaoLoginActivity.class);
+//        startActivity(intent);
+
+//        finish();
     }
 }
